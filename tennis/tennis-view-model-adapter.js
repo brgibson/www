535f179bfa125d328178bb4a9a8a5bcb7aca33df
Benjamin R Gibson <1953625+brgibson@users.noreply.github.com>
2013-09-14 05:15:05 +0000

    tennis - removing player ids - adding player names instead of ids in match synopsis

--- tennis/tennis-view-model-adapter.js
+++ tennis/tennis-view-model-adapter.js
@@ -1,23 +1,23 @@
-var matchViewModelAdapter = function(match) {
+var matchViewModelAdapter = function(match, playerNames) {
 	var matchViewModel = new MatchViewModel()
 		.setScore(match.scores)
-		.setOpponent(match.opponent) //might want to get the actual name here instead of the number
+		.setOpponent(playerNames[match.opponent - 1].playerName)
 		.setPoints(match.points);
 		
 	if (match.scores != "0-0,0-0,0-0") {
 		matchViewModel.setCssClass(match.points <= 8 ? "lose" : "win");
 	} else {
-	    matchViewModel.setCssClass("");
+		matchViewModel.setCssClass("");
 	}
 		
 	return matchViewModel;
 }
 
-var playerViewModelAdapter = function(playerStat) {
+var playerViewModelAdapter = function(playerStat, playerNames) {
 	var matches = [];
 	
 	for (var i = 0; i < playerStat.matches.length; i++) {
-		matches.push(matchViewModelAdapter(playerStat.matches[i]));
+		matches.push(matchViewModelAdapter(playerStat.matches[i], playerNames));
 	}
 
 	return new PlayerViewModel()
@@ -32,7 +32,7 @@ var tennisViewModelAdapter = function(playerStats) {
 	var players = [];
 	
 	for (var i = 0; i < playerStats.length; i++) {
-		players.push(playerViewModelAdapter(playerStats[i]));
+		players.push(playerViewModelAdapter(playerStats[i], playerStats));
 	}
 	
 	return new TennisViewModel().setPlayers(players);
