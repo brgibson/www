24781ac9f2b5a625bab1c40f6dff30e017f00d24
Benjamin R Gibson <1953625+brgibson@users.noreply.github.com>
2013-09-17 02:29:12 +0000

    tennis - small view model cleanup - adding sorting (no interactions)

--- tennis/tennis-view-model-adapter.js
+++ tennis/tennis-view-model-adapter.js
@@ -1,39 +1,59 @@
-var matchViewModelAdapter = function(match, playerNames) {
-	var matchViewModel = new MatchViewModel()
-		.setScore(match.scores)
-		.setOpponent(playerNames[match.opponent - 1].playerName)
-		.setPoints(match.points);
+var TennisViewModelAdapter = function(playerStats) {
+	
+	/**
+	 * Uses the given JSON object representing the players' stats and
+	 * converts it into a TennisViewModel.
+	 */	
+	this.adapt = function() {
+		var players = [];
+	
+		for (var i = 0; i < playerStats.length; i++) {
+			players.push(playerViewModelAdapter(playerStats[i], playerStats));
+		}
 		
-	if (match.scores != "0-0,0-0,0-0") {
-		matchViewModel.setCssClass(match.points <= 8 ? "lose" : "win");
-	} else {
-		matchViewModel.setCssClass("");
-	}
+		//sort the players
+		players.sort(function(a, b) {
+			return b.rating - a.rating;
+		});
 		
-	return matchViewModel;
-}
-
-var playerViewModelAdapter = function(playerStat, playerNames) {
-	var matches = [];
-	
-	for (var i = 0; i < playerStat.matches.length; i++) {
-		matches.push(matchViewModelAdapter(playerStat.matches[i], playerNames));
+		return new TennisViewModel().setPlayers(players);
 	}
 
-	return new PlayerViewModel()
-		.setPlayerId(playerStat.playerId)
-		.setPlayerName(playerStat.playerName)
-		.setPoints(playerStat.totalPoints)
-		.setRating(playerStat.rating)
-		.setMatches(matches);
-}
-
-var tennisViewModelAdapter = function(playerStats) {
-	var players = [];
+	/**
+	 * Takes a JSON object representing a player's stats
+	 * and builds a PlayerViewModel.
+	 */
+	var playerViewModelAdapter = function(playerStat) {
+		var matches = [];
 	
-	for (var i = 0; i < playerStats.length; i++) {
-		players.push(playerViewModelAdapter(playerStats[i], playerStats));
+		for (var i = 0; i < playerStat.matches.length; i++) {
+			matches.push(matchViewModelAdapter(playerStat.matches[i]));
+		}
+	
+		return new PlayerViewModel()
+			.setPlayerId(playerStat.playerId)
+			.setPlayerName(playerStat.playerName)
+			.setPoints(playerStat.totalPoints)
+			.setRating(playerStat.rating)
+			.setMatches(matches);
 	}
 	
-	return new TennisViewModel().setPlayers(players);
-}
+	/**
+	 * Takes a JSON object representing a player's stats and an array
+	 * containing a player names and builds a PlayerViewModel.
+	 */
+	var matchViewModelAdapter = function(match) {
+		var matchViewModel = new MatchViewModel()
+			.setScore(match.scores)
+			.setOpponent(playerStats[match.opponent - 1].playerName) //notice this comes from the TennisViewModelAdapter constructor argument
+			.setPoints(match.points);
+			
+		if (match.scores != "0-0,0-0,0-0") {
+			matchViewModel.setCssClass(match.points <= 8 ? "lose" : "win");
+		} else {
+			matchViewModel.setCssClass("");
+		}
+			
+		return matchViewModel;
+	}
+}	
