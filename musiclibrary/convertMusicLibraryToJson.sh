440240159b0f7e670d4330c1ce52408ed2f7ddc2
Benjamin R Gibson <1953625+brgibson@users.noreply.github.com>
2014-02-11 20:53:23 -0600

    music library - removing first line and last comma from JSON

--- musiclibrary/convertMusicLibraryToJson.sh
+++ musiclibrary/convertMusicLibraryToJson.sh
@@ -2,13 +2,25 @@
 
 mac2unix $1
 
+#remove the first and last line of the input file (these lines don't contain music library data)
+sed '1,1d; $d' $1 > tmpFile
 
-tr '"' "'" < $1 > tmpFile
-sed 's/\([^\t]*\)\t*\(.*\)/["\1","\2"],/' -u tmpFile | uniq -i > tmpFile2
-tr -d '\n' < tmpFile2 > tmpFile3
+#replace all double quotes with single quotes (so we don't break our JSON)
+tr '"' "'" < tmpFile > tmpFile1 
 
+#replace Artist<tab>Album with ["Artist","Album"] and removes duplicates
+sed 's/\([^\t]*\)\t*\(.*\)/["\1","\2"],/' tmpFile1 | uniq -i > tmpFile2
+
+#remove the comma at the end of the last line
+sed '$s/,$//' tmpFile2 > tmpFile3 
+
+#remove all newline characters
+tr -d '\n' < tmpFile3 > tmpFile4
+
+#create the output json file
 echo "var musicLibrary = [" > MusicLibrary.json
-cat tmpFile3 >> MusicLibrary.json
+cat tmpFile4 >> MusicLibrary.json
 echo "\n];\n" >> MusicLibrary.json
 
+#remove the tmp files
 rm tmpFile*
