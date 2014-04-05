63ee7d07d6c08ab6b80cee68d5a23a58062028fe
Benjamin R Gibson <1953625+brgibson@users.noreply.github.com>
2014-04-05 13:56:24 -0500

    contacts - adding option to return contacts as JSON

--- contacts/convertTabularDataToSemanticMarkup.js
+++ contacts/convertTabularDataToSemanticMarkup.js
@@ -39,18 +39,27 @@ var convertTabularDataToSemanticMarkup = (function() {
             getPhone : function() {
                 return getString(/s3.+?s3\">(.+?)<.*/g);
             },
+            getAddressLine1 : function() {
+                return getString(/s3.+?s3.+?s3\">(.+?)<.*/g);
+            },
+            getAddressLine2 : function() {
+                return getString(/s3.+?s3.+?s3.+?s3\">(.+?)<.*/g);
+            },
+            getCity : function() {
+                return getString(/s3.+?s3.+?s3.+?s3.+?s3\">(.+?)<.*/g);
+            },
+            getState : function() {
+                return getString(/s3.+?s3.+?s3.+?s3.+?s3.+?s3\">(.+?)<.*/g);
+            },
+            getZip : function() {
+                return getString(/s4\">(.+?)<.*/g);
+            },
             getAddress : function() {
-                var line1 = getString(/s3.+?s3.+?s3\">(.+?)<.*/g);
-                var line2 = getString(/s3.+?s3.+?s3.+?s3\">(.+?)<.*/g);
-                var city = getString(/s3.+?s3.+?s3.+?s3.+?s3\">(.+?)<.*/g);
-                var state = getString(/s3.+?s3.+?s3.+?s3.+?s3.+?s3\">(.+?)<.*/g);
-                var zip = getString(/s4\">(.+?)<.*/g);
-
                 return "\n    " + 
-                    line1 + "\n    " + 
-                    line2 + "\n    " + 
-                    city + ", " + state + "\n    " + 
-                    zip;
+                    this.getAddressLine1() + "\n    " + 
+                    this.getAddressLine2() + "\n    " + 
+                    this.getCity() + ", " + this.getState() + "\n    " + 
+                    this.getZip();
             },
             getMapUrl : function() {
                 return "#";
@@ -65,9 +74,9 @@ var convertTabularDataToSemanticMarkup = (function() {
     })();
 
     /* an object to build the markup that we want for the entries in our contacts widget */
-    var markupBuilder = (function() {
+    var contactFormatter = (function() {
         /* private variables */
-        var _markup;
+        var _dataParser;
         var _encodeSpecialCharactersFlag;
 
         /* helper function */
@@ -81,37 +90,51 @@ var convertTabularDataToSemanticMarkup = (function() {
 
         /* object to be returned */
         return {
-            init : function(encodeSpecialCharactersFlag) { 
-                _markup = ""; 
+            setDataParser : function(dataParser) { 
+                _dataParser = dataParser; 
+            },
+            setEncodeSpecialCharactersFlag : function(encodeSpecialCharactersFlag) { 
                 _encodeSpecialCharactersFlag = encodeSpecialCharactersFlag;
             },
             getMarkup : function() { 
-                if (_encodeSpecialCharactersFlag) {
-                    return encodeSpecialCharacters(_markup + "\n");    
-                } else {
-                    return _markup;    
-                }
+                var markup = "";
+                markup += "<dt class='name'>"+ _dataParser.getName() +"</dt>\n"; 
+                markup += "<dd class='email'>"+ _dataParser.getEmail() +"</dd>\n"; 
+                markup += "<dd class='phone'>"+ _dataParser.getPhone() +"</dd>\n"; 
+                markup += "<dd class='address'>"+ _dataParser.getAddress() +"\n</dd>\n"; 
+                markup += "<dd><a href='"+ _dataParser.getMapUrl() +"'>map</a></dd>\n"; 
+                markup += "<dd><a href='"+ _dataParser.getChatsUrl() +"'>Chats</a></dd>\n"; 
+                markup += "<dd><a href='"+ _dataParser.getEmailsUrl() +"'>Emails</a></dd>\n"; 
+               
+                return _encodeSpecialCharactersFlag ? encodeSpecialCharacters(markup + "\n") : markup;    
             },
-            addName : function(name) { 
-                _markup += "<dt class='name'>"+ name +"</dt>\n"; 
-            },
-            addEmail : function(email) { 
-                _markup += "<dd class='email'>"+ email +"</dd>\n"; 
-            },
-            addPhone : function(phone) { 
-                _markup += "<dd class='phone'>"+ phone +"</dd>\n"; 
-            },
-            addAddress : function(address) { 
-                _markup += "<dd class='address'>"+ address +"\n</dd>\n"; 
-            },
-            addMapUrl : function(mapUrl) { 
-                _markup += "<dd><a href='"+ mapUrl +"'>map</a></dd>\n"; 
-            },
-            addChatsUrl : function(chatsUrl) { 
-                _markup += "<dd><a href='"+ chatsUrl +"'>Chats</a></dd>\n"; 
-            },
-            addEmailsUrl : function(emailsUrl) { 
-                _markup += "<dd><a href='"+ emailsUrl +"'>Emails</a></dd>\n"; 
+            getJSON : function() {
+                var formatJSONString = function(key, value, omitComma) {
+                    var str = '"'+ key +'":"'+ value +'"';
+                    return omitComma ? str: str  + ",";
+                };
+                var formatJSONObject = function(key, value, omitComma) {
+                    var str = '"'+ key +'":'+ value +'';
+                    return omitComma ? str: str  + ",";
+                };
+                
+                var json = "{";
+                json += formatJSONString("name", _dataParser.getName());
+                json += formatJSONString("email", _dataParser.getEmail());
+                json += formatJSONString("phone", _dataParser.getPhone());
+                json += formatJSONObject("address", "{" 
+                                    + formatJSONString("line1", _dataParser.getAddressLine1())
+                                    + formatJSONString("line2", _dataParser.getAddressLine2())
+                                    + formatJSONString("city", _dataParser.getCity())
+                                    + formatJSONString("state", _dataParser.getState())
+                                    + formatJSONString("zip", _dataParser.getZip(), "omitComma")
+                                    + "}");
+                json += formatJSONString("mapUrl", _dataParser.getMapUrl());
+                json += formatJSONString("chatsUrl", _dataParser.getChatsUrl());
+                json += formatJSONString("emailsUrl", _dataParser.getEmailsUrl(), "omitComma");
+                json += "}";
+                
+                return _encodeSpecialCharactersFlag ? encodeSpecialCharacters(json + "\n") : json;    
             }
         };            
 
@@ -121,22 +144,27 @@ var convertTabularDataToSemanticMarkup = (function() {
     /**
      * Return the function we will be using to convert the data.
      */
-    return function(str, encodeSpecialCharactersFlag) {
-        var markup = "";
+    return function(str, encodeSpecialCharactersFlag, asJSON) {
+        
+        var formattedContacts = asJSON ? "[" : "";
+        
         for (var i = 1, entries = str.split("<tr>"); i < entries.length ; i++) {
             dataParser.setDataToParse(entries[i]);
 
-            markupBuilder.init(encodeSpecialCharactersFlag);
-            markupBuilder.addName(dataParser.getName());
-            markupBuilder.addEmail(dataParser.getEmail());
-            markupBuilder.addPhone(dataParser.getPhone());
-            markupBuilder.addAddress(dataParser.getAddress());
-            markupBuilder.addMapUrl(dataParser.getMapUrl());
-            markupBuilder.addChatsUrl(dataParser.getChatsUrl());
-            markupBuilder.addEmailsUrl(dataParser.getEmailsUrl());
-
-            markup += markupBuilder.getMarkup();
+            contactFormatter.setDataParser(dataParser);
+            contactFormatter.setEncodeSpecialCharactersFlag(encodeSpecialCharactersFlag);
+            
+            formattedContacts += asJSON 
+                ? contactFormatter.getJSON() + ","
+                : contactFormatter.getMarkup();
         }
-        return markup;
+        
+        if (formattedContacts.length > 1) {
+            return asJSON 
+                ? formattedContacts.substring(0, formattedContacts.length - 1) + "]"
+                : formattedContacts;
+        } else {
+            return "";
+        }
     };
 })();
