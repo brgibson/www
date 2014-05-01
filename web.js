d85cf3ac2abe43a3128d7a559af8d21d3e9848a6
Benjamin R Gibson <1953625+brgibson@users.noreply.github.com>
2014-04-30 21:21:39 -0500

    nodejs - pointing web.js to my static site

--- web.js
+++ web.js
@@ -5,9 +5,12 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
+/*
 app.get('/', function(req, res) {
 	res.send('Hello World!');
     });
+*/
+app.use(express.static(__dirname + '/'));
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
