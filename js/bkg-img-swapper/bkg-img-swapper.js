0f97680716bbf7f29109df8cb2fb62bb425e927d
Benjamin R Gibson <1953625+brgibson@users.noreply.github.com>
2015-06-16 22:53:35 -0700

    redesign - hero image is no long 'fixed' - album backgrounds wrap facebook posts

---
+++ js/bkg-img-swapper/bkg-img-swapper.js
@@ -0,0 +1,4 @@
+(function () {
+    var container = document.getElementById("hero-img-container");
+    container.style.background = "url(/photos/color/Blacklight-Large.png) no-repeat center center";
+})();
