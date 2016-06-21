c7225d48caf2bba9934351447f439bd511909fb5
Benjamin R Gibson <1953625+brgibson@users.noreply.github.com>
2016-06-20 23:19:52 -0700

    jekyll - migrating majority of pages

--- _modules/_controllers/index.js
+++
@@ -1,8 +0,0 @@
-var albums = require('../_models/albums.js');
-
-exports.controller = function(req, res) {
-    res.render('index', {
-        title: 'page title',
-        albumSection: albums.albums
-    });
-};
